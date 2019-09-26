86a9834af653180985c4ed0a7ffbd8c6b841135f
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-26 05:07:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,6 +5,10 @@ class Student
   attr_reader :id
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
 
 end
