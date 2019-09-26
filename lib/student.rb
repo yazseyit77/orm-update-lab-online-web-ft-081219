866e189faaa12067da7413e410cf0c882d538f89
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-26 05:20:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,7 +55,12 @@ class Student
     student
   end
 
-  def self.new_from_db
+  def self.new_from_db(row)
+    student = self.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
 
   end
 
