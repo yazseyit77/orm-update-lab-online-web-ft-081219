615e57788969e0709500c2ed780add45ee63001d
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-26 05:22:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,6 +62,12 @@ class Student
   end
 
   def self.find_by_name(name)
+    sql = <<-SQL
+        SELECT * FROM Students
+        WHERE name = ?
+        LIMIT 1
+      SQL
 
+    DB[:conn].execute(sql, name).map{|row| self.new_from_db(row)}.first
   end
 end
