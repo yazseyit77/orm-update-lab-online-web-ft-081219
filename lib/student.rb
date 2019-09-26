fad121116cf70b256f53f93ec7b1f0a5266d202c
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-26 05:17:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,7 +31,7 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  def self.update
+  def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.album, self.id)
   end
