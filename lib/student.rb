287765d3641bb6afa3e3fc77d897da3f763e3baa
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-26 05:14:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,7 +40,7 @@ class Student
         VALUES (?, ?)
       SQL
       DB[:conn].execute(sql, self.name, self.grade)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
   end
 
